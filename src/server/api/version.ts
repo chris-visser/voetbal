2836f80a26a8f02acb5edc88da3aef38948df80b
The Spider <2496739+chris-visser@users.noreply.github.com>
2023-09-09 11:36:48 +0200

    Log ping and clientname

--- src/server/api/version.ts
+++ src/server/api/version.ts
@@ -1,11 +1,26 @@
 // import { getProgram } from '~/server/client'
 
-export default defineEventHandler(async (event) => {
+const ipMap: Record<string, string> = {
+    '::ffff:127.0.0.1': 'Thuis (development)',
+    '3.82.96.116': 'SV de Rijp',
+    '83.84.2.172': 'Thuis'
+}
 
-    console.log('Version ping from', event.headers)
+const VERSION = 'v0.0.4-beta'
+
+export default defineEventHandler(async (event) => {
+    const forwardedFor = getHeader(event, 'x-forwarded-for')
+    if(!forwardedFor) {
+        return {
+            version: VERSION
+        }
+    }
+    
+    const clientName = ipMap[forwardedFor] || 'Onbekend'
+    console.log(`Version ping from "${clientName}" `)
     // const storage = useStorage('redis')
 
     return {
-        version: 'v0.0.4-beta'
+        version: VERSION
     }
   })
