28e6ca2e50982cce45a1dcd6fb5fe0b5859e55c8
The Spider <2496739+chris-visser@users.noreply.github.com>
2023-09-22 17:13:47 +0200

    Bump version, triggering refresh of screen

--- src/server/api/version.ts
+++ src/server/api/version.ts
@@ -7,7 +7,7 @@ const ipMap: Record<string, string> = {
     '83.84.2.172': 'Thuis'
 }
 
-const VERSION = 'v0.0.6-beta'
+const VERSION = 'v0.0.7'
 
 export default defineEventHandler(async (event) => {
     const forwardedFor = getHeader(event, 'x-forwarded-for')
