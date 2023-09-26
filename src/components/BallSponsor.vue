7a576901f8c21d85fb1066dea58d5fbaac425c50
The Spider <2496739+chris-visser@users.noreply.github.com>
2023-09-26 13:48:37 +0200

    Update sponsor

--- src/components/BallSponsor.vue
+++ src/components/BallSponsor.vue
@@ -1,17 +1,35 @@
 <template>
   <section class="p-8 bg-white rounded-md flex flex-col items-center">
     <h2 class="font-bold text-3xl text-center mb-5 text-primary-400">Sponsor wedstrijdbal</h2>
-    <img src="/restaurant-oudejans.webp" class="h-[150px] inline-block mb-4"
-         alt="Sponsor wedstrijdbal - Restaurant Oudejans"/>
-    <p class="text-center max-w-[400px]">
-      De wedstrijdbal van het 1e elftal wordt gesponsord door
-      <strong>Restaurant Oudejans</strong> - de perfecte plek voor een goede
-      lunch,
-      high tea of tapas/diner. De serre met eigen bar en toiletgroep is ideaal voor besloten partijen.
-    </p>
-    <h3 class="font-bold text-lg my-4">Voor meer info. Scan de QR code</h3>
-    <img src="/restaurant-oudejans-qr.png" class="h-[250px]" />
+    <img src="/balsponsor.jpeg" class="h-[200px] inline-block mb-4"
+         alt="Sponsor wedstrijdbal - Schildersbedrijf de Tapperij"/>
 
-    <p class="text-sm mt-4">www.restaurantoudejans.nl</p>
+    <h3 class="text-2xl font-bold my-8">Schildersbedrijf de Tapperij</h3>
+
+    <p class="max-w-[400px] my-8 text-justify">
+      Sinds 2005 is Schildersbedrijf De Tapperij bezig als schilder en is zodoende 18 jaar bezig binnen dit domein.
+      Dit schildersbedrijf kan je dan ook goed van dienst zijn bij bijvoorbeeld Het schilderen van kozijnen.
+    </p>
+
+    <dl class="grid grid-cols-2 text-lg">
+      <dt class="font-semibold">E-mailadres</dt>
+      <dd>verftapperij@xs4all.nl</dd>
+      <dt class="font-semibold">Contactpersoon</dt>
+      <dd>Dhr. E. Roele</dd>
+      <dt class="font-semibold">Telefoon</dt>
+      <dd>06-50501325</dd>
+    </dl>
+<!--    <p class="text-center max-w-[400px]">-->
+<!--      De wedstrijdbal van het 1e elftal wordt gesponsord door-->
+<!--      <strong>Restaurant Oudejans</strong> - de perfecte plek voor een goede-->
+<!--      lunch,-->
+<!--      high tea of tapas/diner. De serre met eigen bar en toiletgroep is ideaal voor besloten partijen.-->
+<!--    </p>-->
+<!--    <h3 class="font-bold text-lg my-4">Voor meer info. Scan de QR code</h3>-->
+<!--    <img src="/restaurant-oudejans-qr.png" class="h-[250px]" />-->
+
+<!--    <p class="text-sm mt-4">www.restaurantoudejans.nl</p>-->
   </section>
 </template>
+<script setup lang="ts">
+</script>
