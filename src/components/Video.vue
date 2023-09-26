31b2f238fa8c23398ec16f1c9d7156dd34d07166
The Spider <2496739+chris-visser@users.noreply.github.com>
2023-09-26 15:23:19 +0200

    Fix video stutter on Pi?

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const url = 'https://dq7qi3p659m75.cloudfront.net/drone-shot-sv-de-rijp.MP4'
+const url = 'https://dq7qi3p659m75.cloudfront.net/drone-sv-de-rijp.webm'
 // const src = ref('')
 //
 // onMounted(() => {
@@ -14,8 +14,8 @@ const url = 'https://dq7qi3p659m75.cloudfront.net/drone-shot-sv-de-rijp.MP4'
 </script>
 
 <template>
-  <video autoplay muted loop ref="video" class="min-h-screen aspect-video object-cover">
-    <source :src="url" type="video/mp4">
+  <video autoplay muted loop ref="video" class="min-h-screen min-w-full aspect-video object-cover">
+    <source :src="url" type="video/webm">
     <!-- Add additional source elements for different video formats (WebM, Ogg) -->
   </video>
 </template>
