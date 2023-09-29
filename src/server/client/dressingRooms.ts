ca576abdca4e24f8c22a26f4cc0d12c79f386235
The Spider <2496739+chris-visser@users.noreply.github.com>
2023-09-29 21:11:53 +0200

    Grote clubactie

---
+++ src/server/client/dressingRooms.ts
@@ -0,0 +1,41 @@
+const sides = [{
+    label: 'L',
+    description: 'Links'
+},{
+    label: 'R',
+    description: 'Rechts'
+}]
+
+export const dressingRooms = [{
+    number: 1,
+    name: 'Wapen van Munster',
+    sides: sides
+}, {
+    number: 2,
+    name: 'Oudejans',
+    sides
+}, {
+    number: 3,
+    name: 'LWM-Ned',
+    sides
+}, {
+    number: 4,
+    name: 'Medec',
+    sides
+}, {
+    number: 5,
+    name: 'ING',
+    sides
+}, {
+    number: 6,
+    name: 'Davibouw',
+    sides
+}, {
+    number: 7,
+    name: 'Dekker BV',
+    sides
+}, {
+    number: 8,
+    name: 'Dijkstra Kolbe',
+    sides
+}]
