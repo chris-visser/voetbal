df5c43acefaba55f441b81962ab68b686a5100c8
The Spider <2496739+chris-visser@users.noreply.github.com>
2023-11-11 20:35:21 +0100

    Add ballsponsor Skybox

--- src/components/BallSponsor.vue
+++ src/components/BallSponsor.vue
@@ -1,69 +1,55 @@
 <template>
   <section class="p-8 bg-white rounded-md flex flex-col items-center">
 
-<!--    <h2 class="font-bold text-3xl text-center mb-5 text-primary-400">Grote clubactie</h2>-->
-<!--    <img src="/grote-clubactie.png" class="h-[200px] inline-block"-->
-<!--         alt="Grote clubactie"/>-->
+    <!--    <h2 class="font-bold text-3xl text-center mb-5 text-primary-400">Grote clubactie</h2>-->
+    <!--    <img src="/grote-clubactie.png" class="h-[200px] inline-block"-->
+    <!--         alt="Grote clubactie"/>-->
 
-<!--    <h3 class="text-2xl font-bold my-8">Grote clubactie gestart</h3>-->
+    <!--    <h3 class="text-2xl font-bold my-8">Grote clubactie gestart</h3>-->
 
-<!--    <p class="max-w-[400px] my-4 text-justify">-->
-<!--      Voor elk verkocht lot van € 3,- krijgt SV de Rijp 80%, dit betekent-->
-<!--      dus dat als jij 20 loten weet te verkopen er € 48,- (!!) naar de clubkas gaat. Helemaal mooi is dat de opbrengst-->
-<!--      voor de jeugd wordt gebruikt. Dit jaar willen wij de coaches graag voorzien van ballentassen,-->
-<!--      waterzakken en bidons.-->
-<!--    </p>-->
+    <!--    <p class="max-w-[400px] my-4 text-justify">-->
+    <!--      Voor elk verkocht lot van € 3,- krijgt SV de Rijp 80%, dit betekent-->
+    <!--      dus dat als jij 20 loten weet te verkopen er € 48,- (!!) naar de clubkas gaat. Helemaal mooi is dat de opbrengst-->
+    <!--      voor de jeugd wordt gebruikt. Dit jaar willen wij de coaches graag voorzien van ballentassen,-->
+    <!--      waterzakken en bidons.-->
+    <!--    </p>-->
 
-<!--    <h3 class="text-center font-semibold text-lg mb-4">Doe je mee?</h3>-->
-<!--    <p class="max-w-[400px] text-center">-->
-<!--      Ga naar <a href="https://www.clubactie.nl" class="underline text-primary">www.clubactie.nl</a> of scan de onderstaande QR code!-->
-<!--      <img src="/qr-grote-clubactie.png" class="block w-[200px] m-auto" alt="QR grote www.clubactie.nl" />-->
-<!--    </p>-->
-
-<!--    <dl class="grid grid-cols-2 text-lg">-->
-<!--      <dt class="font-semibold">E-mailadres</dt>-->
-<!--      <dd>verftapperij@xs4all.nl</dd>-->
-<!--      <dt class="font-semibold">Contactpersoon</dt>-->
-<!--      <dd>Dhr. E. Roele</dd>-->
-<!--      <dt class="font-semibold">Telefoon</dt>-->
-<!--      <dd>06-50501325</dd>-->
-<!--    </dl>-->
-
-    <!--<h2 class="font-bold text-3xl text-center mb-5 text-primary-400">Sponsor wedstrijdbal</h2>
-    <img src="/balsponsor.jpeg" class="h-[200px] inline-block mb-4"
+    <h2 class="font-bold text-3xl text-center mb-5 text-primary-400">Sponsor wedstrijdbal</h2>
+    <img src="/balsponsor.jpeg" class="h-[200px] inline-block mb-2"
          alt="Sponsor wedstrijdbal"/>
 
-    <h3 class="text-2xl font-bold my-8">F-Side (supportersgroep)</h3>
+    <h3 class="text-2xl font-bold mt-2 mb-2 text-center">Skybox<br />(vriendengroep Tuur Boerkool)</h3>
 
-    <p class="max-w-[400px] my-8 text-justify">
-      Zondag 29 oktober om 14.30 uur speelt ons vlaggenschip thuis tegen ALC. De wedstrijdbal voor dit 
-      duel wordt geschonken door de trouwe supporters van de F-Side.
+    <p class="max-w-[400px] my-2 text-justify">
+      Zondag 12 november speelt het eerste elftal om 14:30 uur thuis tegen Hauwert. De wedstrijdbal
+      wordt dit duel gesponsord door de vriendengroep van Tuur Boerkool, genoemd ‘Skybox’.
     </p>
-  -->
-  <h2 class="font-bold text-3xl text-center mb-5 text-primary-400">Algemene ledenvergadering</h2>
-
-  <p class="max-w-[400px] my-8 text-justify">
-    Hierbij nodigt het bestuur alle leden en ouders van jeugdleden uit voor het bijwonen van de 
-    Jaarlijkse Algemene Ledenvergadering op donderdag 30 november 2023.
-  </p>
 
 
-  <p class="font-bold max-w-[400px] my-8 text-center">
-    Aanvang: 20:00 uur<br />
-    Locatie: kantine SV De Rijp
-  </p>
+    <h2 class="font-bold text-3xl text-center mb-4 mt-4 text-primary-400">Algemene ledenvergadering</h2>
+
+    <p class="max-w-[400px] mb-2 bt-4 text-justify">
+      Hierbij nodigt het bestuur alle leden en ouders van jeugdleden uit voor het bijwonen van de
+      Jaarlijkse Algemene Ledenvergadering op donderdag 30 november 2023.
+    </p>
 
 
-<!--    <p class="text-center max-w-[400px]">-->
-<!--      De wedstrijdbal van het 1e elftal wordt gesponsord door-->
-<!--      <strong>Restaurant Oudejans</strong> - de perfecte plek voor een goede-->
-<!--      lunch,-->
-<!--      high tea of tapas/diner. De serre met eigen bar en toiletgroep is ideaal voor besloten partijen.-->
-<!--    </p>-->
-<!--    <h3 class="font-bold text-lg my-4">Voor meer info. Scan de QR code</h3>-->
-<!--    <img src="/restaurant-oudejans-qr.png" class="h-[250px]" />-->
+    <p class="font-bold max-w-[400px] my-2 text-center">
+      Aanvang: 20:00 uur<br/>
+      Locatie: kantine SV De Rijp
+    </p>
 
-<!--    <p class="text-sm mt-4">www.restaurantoudejans.nl</p>-->
+
+    <!--    <p class="text-center max-w-[400px]">-->
+    <!--      De wedstrijdbal van het 1e elftal wordt gesponsord door-->
+    <!--      <strong>Restaurant Oudejans</strong> - de perfecte plek voor een goede-->
+    <!--      lunch,-->
+    <!--      high tea of tapas/diner. De serre met eigen bar en toiletgroep is ideaal voor besloten partijen.-->
+    <!--    </p>-->
+    <!--    <h3 class="font-bold text-lg my-4">Voor meer info. Scan de QR code</h3>-->
+    <!--    <img src="/restaurant-oudejans-qr.png" class="h-[250px]" />-->
+
+    <!--    <p class="text-sm mt-4">www.restaurantoudejans.nl</p>-->
   </section>
 </template>
 <script setup lang="ts">
