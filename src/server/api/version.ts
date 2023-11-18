389ba12e69f250f73a98102aa1e759547f1f6c7c
The Spider <2496739+chris-visser@users.noreply.github.com>
2023-11-18 08:45:49 +0100

    Bump version

--- src/server/api/version.ts
+++ src/server/api/version.ts
@@ -7,7 +7,7 @@ const ipMap: Record<string, string> = {
     '83.84.2.172': 'Thuis'
 }
 
-const VERSION = 'v0.4.4'
+const VERSION = 'v0.4.5'
 
 export default defineEventHandler(async (event) => {
     const forwardedFor = getHeader(event, 'x-forwarded-for')
