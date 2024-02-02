5b5b2624ef79e8b46a86e99d2fefb4c5ad71957a
The Spider <2496739+chris-visser@users.noreply.github.com>
2024-02-02 16:58:38 +0100

    New article

--- src/components/BallSponsor.vue
+++ src/components/BallSponsor.vue
@@ -1,50 +1,25 @@
 <template>
   <section class="p-8 bg-white rounded-md flex flex-col items-center">
 
-    <!--    <h2 class="font-bold text-3xl text-center mb-5 text-primary-400">Grote clubactie</h2>-->
-    <!--    <img src="/grote-clubactie.png" class="h-[200px] inline-block"-->
-    <!--         alt="Grote clubactie"/>-->
-
-    <!--    <h3 class="text-2xl font-bold my-8">Grote clubactie gestart</h3>-->
-
-    <!--    <p class="max-w-[400px] my-4 text-justify">-->
-    <!--      Voor elk verkocht lot van € 3,- krijgt SV de Rijp 80%, dit betekent-->
-    <!--      dus dat als jij 20 loten weet te verkopen er € 48,- (!!) naar de clubkas gaat. Helemaal mooi is dat de opbrengst-->
-    <!--      voor de jeugd wordt gebruikt. Dit jaar willen wij de coaches graag voorzien van ballentassen,-->
-    <!--      waterzakken en bidons.-->
-    <!--    </p>-->
-
-    <!--    <h2 class="font-bold text-3xl text-center mb-5 text-primary-400">Sponsor wedstrijdbal</h2>-->
-    <!--    <img src="/balsponsor.jpeg" class="h-[200px] inline-block mb-2"-->
-    <!--         alt="Sponsor wedstrijdbal"/>-->
-
-    <!--    <h3 class="text-2xl font-bold mt-2 mb-2 text-center">Skybox<br />(vriendengroep Tuur Boerkool)</h3>-->
-
-    <!--    <p class="max-w-[400px] my-2 text-justify">-->
-    <!--      Zondag 12 november speelt het eerste elftal om 14:30 uur thuis tegen Hauwert. De wedstrijdbal-->
-    <!--      wordt dit duel gesponsord door de vriendengroep van Tuur Boerkool, genoemd ‘Skybox’.-->
-    <!--    </p>-->
-
-
     <h2 class="font-bold text-3xl text-center mb-8 mt-4 text-primary-400">
-      Ter nagedachtenis: <br />Oud-trainer Junior Pourier
+      Wij zoeken vrijwilligers<br />voor het jeugdbestuur
     </h2>
 
-    <img src="/junior.jpg" class="h-[250px] inline-block mb-6" alt="Junior Pourier" />
+    <img src="/jeugd.jpg" class="max-w-[400px] mb-8" />
 
     <p class="max-w-[400px] mb-4 bt-8\4 text-justify">
-      Geschokt heeft SV De Rijp kennisgenomen van het plotseling overlijden van Junior Pourier.
-      Afgelopen woensdag is hij op 57-jarige leeftijd overleden na een kort ziekbed.
+      Voor het jeugdbestuur van SV De Rijp zijn we op zoek naar vrijwilligers! In de afgelopen jaren is een 
+      te kort aan ondersteuning ontstaan. Het jeugdbestuur bij SV De Rijp faciliteert een goed verloop van voetbal 
+      op zaterdag, werving van jeugdleden, zorgt dat niemand buitenspel staat en organiseert leuke activiteiten 
+      gedurende het voetbalseizoen. Om de bezetting goed te houden, zoeken wij duurzame versterking voor meerdere jaren.
     </p>
 
+    <h3 class="max-w-[400px] mb-4 font-bold">Enthousiast?</h3>
+
     <p class="max-w-[400px] mb-4 bt-4 text-justify">
-      Junior is jeugdtrainer geweest bij SV De Rijp en heeft verschillende jongens van de huidige
-      selectie onder zijn hoede gehad. Daarnaast heeft hij zich ook ingezet voor het trainen van de dames en de (oude)
-      zaterdag 2.
-    </p>
-    <p class="max-w-[400px] bt-4 text-justify">
-      Ter nagedachtenis zal er voor aanvang van de wedstrijd van ons eerste elftal tegen Kwiek ’78 om 14.30 uur een minuut
-      stilte worden gehouden.
+      Meld je dan aan voor een van de beschikbare gebieden! Neem contact op met Tom Smits (voorzitter Jeugdbestuur) via 
+      t.smits01@outlook.com of 0681792270 of met Nick Blokdijk (secretaris Dagelijks Bestuur) via secretaris@svderijp.nl
+      of 0634299526.
     </p>
 
 
--- src/server/api/version.ts
+++ src/server/api/version.ts
@@ -8,7 +8,7 @@ const ipMap: Record<string, string> = {
     '83.84.2.172': 'Thuis'
 }
 
-const VERSION = 'v0.4.7'
+const VERSION = 'v0.4.8'
 
 export default defineEventHandler(async (event) => {
     const forwardedFor = getHeader(event, 'x-forwarded-for')
