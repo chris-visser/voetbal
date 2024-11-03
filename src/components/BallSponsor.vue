437aad675457f89087f680091a837028543f00a5
Chris Visser <2496739+chris-visser@users.noreply.github.com>
2024-11-03 09:50:46 +0100

    Padding tweaks

--- src/components/BallSponsor.vue
+++ src/components/BallSponsor.vue
@@ -1,42 +1,42 @@
 <template>
   <section class="p-8 bg-white rounded-md items-center">
-    <h2 class="font-bold text-3xl mb-4 text-primary-400">
-      Gezocht:<br>
+    <h2 class="font-bold text-2xl mb-4 text-primary-400">
+      Gezocht:
       Versterking van het Technisch Hart!
     </h2>
 
-    <p class="mb-4 bt-8\4 text-justify">
+    <p class="mb-3 bt-8\4 text-justify">
       Als Technisch Hart van SV De Rijp willen we de taken meer gaan verdelen “bredere schouders” creëren. Daarom zijn we op zoek naar hulp voor de volgende 2 functies:
     </p>
 
-    <h3 class="mb-4 font-bold w-full">
+    <h3 class="mb-3 font-bold w-full">
       Coördinator Fundament/Onderbouw (JO8-JO12)
     </h3>
 
-    <ul class="mb-4 bt-4 pl-4 list-disc">
+    <ul class="mb-3 bt-4 pl-4 list-disc">
       <li>Aanspreekpunt voor trainers en leiders Fundament/Onderbouw (JO8-JO12; 9 teams)</li>
       <li>Brugfunctie naar TH; vergaderingen TH (6x per jaar) bijwonen en deelname aan de groepsapp van TH</li>
     </ul>
 
-    <h3 class="mb-4 font-bold w-full">
+    <h3 class="mb-3 font-bold w-full">
       Coördinator Midden-/Bovenbouw (JO13-JO23)
     </h3>
 
-    <ul class="mb-4 bt-4 pl-4 list-disc">
+    <ul class="mb-3 bt-4 pl-4 list-disc">
       <li>Aanspreekpunt voor trainers en leiders Midden-/Bovenbouw (JO13-JO23; 6 teams)</li>
       <li>Brugfunctie naar TH; vergaderingen (6x per jaar) bijwonen en deelname aan de groepsapp van TH</li>
     </ul>
 
-    <p class="mb-4 bt-4">
+    <p class="mb-3 bt-4">
       <strong>Wat doe je WEL:</strong> vraagbaak voor trainingen en wedstrijden, oefenstof, evaluatie trainers samen met Hoofd jeugdopleiding, advies aan Hoofd jeugdopleiding over team-indelingen.
     </p>
-    <p class="mb-4 bt-4">
+    <p class="mb-3 bt-4">
       <strong>Wat doe je NIET:</strong> direct contact met alle ouders van de betreffende bouw (dit loopt altijd via de trainer), tekort aan spelers op wedstrijddag (dat doen trainers zelf), kledingzaken, scheidsrechters, kantinediensten etc.
     </p>
     <h3 class="font-bold w-full">
       Ben je enthousiast of wil je meer weten?
     </h3>
-    <p class="mb-4 bt-4">
+    <p class="mb-3 bt-4">
       Meld je aan bij Patrick Oudejans (<span class="text-red-600">oudejansp@gmail.com</span>).
     </p>
   </section>
