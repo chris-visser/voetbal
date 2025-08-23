6c9f633eb3eaaf3708a808013b40bc43d1b8c7a7
Chris Visser <2496739+chris-visser@users.noreply.github.com>
2025-08-23 17:33:57 +0200

    Revise discount offer text in BallSponsor component

--- src/components/BallSponsor.vue
+++ src/components/BallSponsor.vue
@@ -1,11 +1,11 @@
-<template>
+d<template>
   <section class="p-8 bg-white rounded-md items-center">
     <h2 class="font-bold text-2xl mb-4 text-primary-400">
       Begin het seizoen goed, korting bij Duijn Teamsport!
     </h2>
 
     <p class="mb-3 bt-8\4 text-justify">
-      Om dit jaar sterk van start te gaan, biedt Duijn Teamsport een exclusieve kortingscode aan. Deze is geldig t/m 30 september 2025.
+      Duijn Teamsport biedt een exclusieve kortingscode aan. 
     </p>
 
    <img src="https://website.storage/Data/Rijp/RTE/Afbeeldingen/NieuwsItem/237/SSA-De-Rijp-2025.jpg" class="w-full" />
